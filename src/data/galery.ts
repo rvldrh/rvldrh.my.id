0b0321970c28f93f6588e47304b52bc2e04373ca
rvldrh <rivaldo0347t@gmail.com>
2026-08-12 20:35:54 +0700

    feat: adding video to gallery section

--- src/data/galery.ts
+++ src/data/galery.ts
@@ -1,19 +1,33 @@
-
-
 export const galleryItems = [
-  {
-    type: "image",
-    src: "/gallery/project/sportline.png",
-    alt: "Website Company Profile Screenshot",
-    title: "Sportline",
-    description: "Website Grade 11 Level Up Exam",
-  },
   {
     type: "video",
     src: "/gallery/project/final.mp4",
     alt: "Demo Video of E-Commerce App",
     title: "E-Commerce App",
     description: "A demo video showcasing the features of the E-Commerce App.",
+  },
+  {
+    type: "video",
+    src: "/gallery/project/final-ddap.mp4",
+    alt: "Demo Video of UniFind Lost and Found Platform",
+    title: "UniFind",
+    description:
+      "A UI/UX academic project developed as a final assignment for the Fundamentals of User Interface Design course. UniFind is a concept platform for reporting and finding lost items, designed as a non-production prototype to explore user flows, interface design, and usability.",
+  },
+  {
+    type: "video",
+    src: "/gallery/project/bcc-2025.mp4",
+    alt: "Demo Video of Carevo Career Planning Platform",
+    title: "Carevo",
+    description:
+      "A frontend selection project for Basic Computing Community (BCC) 2025. Carevo is a career planning platform designed to help users explore career paths, generate ATS-friendly CVs with AI based on their profile and experience, and receive personalized recommendations for career tracks such as UI/UX, Frontend, and Backend, along with relevant bootcamps and certifications.",
+  },
+  {
+    type: "image",
+    src: "/gallery/project/sportline.png",
+    alt: "Website Company Profile Screenshot",
+    title: "Sportline",
+    description: "Website Grade 11 Level Up Exam",
   },
   {
     type: "image",
@@ -62,14 +76,16 @@ export const galleryItems = [
     src: "/gallery/project/react-storybook.png",
     alt: "Website Company Profile Screenshot",
     title: "React Storybook",
-    description: "Imploementation of React Storybook in Industrial Class with Machine Vision Indonesia",
+    description:
+      "Imploementation of React Storybook in Industrial Class with Machine Vision Indonesia",
   },
   {
     type: "image",
     src: "/gallery/project/toko-online.png",
     alt: "Website Company Profile Screenshot",
     title: "Toko Online",
-    description: "online store website for class 11 level up exam using native php",
+    description:
+      "online store website for class 11 level up exam using native php",
   },
   // Tambahkan lebih banyak item sesuai kebutuhan
 ];
